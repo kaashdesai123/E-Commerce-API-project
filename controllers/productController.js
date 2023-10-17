610209debf08e432b76cc1536aa56a6c3428104d
Kaash Desai <138083663+kaashdesai123@users.noreply.github.com>
2023-10-16 20:46:22 -0700

    Create productController.js

---
+++ controllers/productController.js
@@ -0,0 +1,12 @@
+const Product = require('../models/product');
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Add more CRUD operations here
